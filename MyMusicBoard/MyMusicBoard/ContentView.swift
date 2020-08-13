062c5354eb0e3781e20238ba68c5c09d03863f71
pete <pete.victoratos@aboutobjects.com>
2020-08-13 17:34:07 -0400

    gutted old stuff, added current UI and lots of to-dos
    feeling reaaaaaally good about this

--- MyMusicBoard/MyMusicBoard/ContentView.swift
+++ MyMusicBoard/MyMusicBoard/ContentView.swift
@@ -1,128 +1,95 @@
 //
 //  ContentView.swift
-//  MyMusicBoard
+//  circles
 //
-//  Created by Peter Victoratos on 8/6/20.
+//  I would like an application where users can easily highlight the songs they are listening to, weekly. The hopes is that users can look back on their calendar and "relive" feelings via the music they were listening to and share this with others.
+//
+//  Created by Peter Victoratos on 8/11/20.
 //
 
 import SwiftUI
 
-//make a struct that has 7 circles, in a circle
-
 struct ContentView: View {
+    static var numCircles = 7
+    static var tau = CGFloat(2.0 * 3.14159265358979323846)
+    static var fourFifths = CGFloat(0.8)
+    static var circleSize = fourFifths/10.0
+    
     var body: some View {
-        ZStack {
-            Image("userImage").clipShape(Circle()).shadow(radius: 10).overlay(Circle().stroke(Color.red, lineWidth: 5)).scaleEffect(0.3, anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            SongCircle().scaleEffect(0.7)
-        }
+            GeometryReader { boundingBox in
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        ZStack{
+                            Image("shneaky")
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                .shadow(radius:10)
+                                .overlay(Circle()
+                                            .stroke(Color.white, lineWidth: 5))
+                                .scaleEffect(ContentView.fourFifths - 0.3, anchor: .center)
+                        GeometryReader { square in
+                            ForEach(0 ..< ContentView.numCircles) { i in
+                                //replace these circles with buttons that allow user to click into and get more data, a hero animation would be great
+                                Image("spoofy")
+                                    .frame(
+                                        width: boundingBox.size.width * ContentView.circleSize + 170, //translates on x
+                                        height: boundingBox.size.width * ContentView.circleSize + 15, //scales
+                                        alignment: .center
+                                    ) //circle size
+                                    .scaleEffect(0.025)
+                                    .offset(self.position(boundingBox, for: i))
+                            }
+                        }
+                        }
+                        .frame(
+                            width: ContentView.fourFifths * boundingBox.size.width + 10,
+                            height: ContentView.fourFifths * boundingBox.size.width + 10,
+                            alignment: .center
+                        ) // box size
+                        Spacer()
+                    }
+                    Text("shneakypete")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .offset(x: 0.0 , y: 20)
+                    Spacer()
+                    Button(action: {}) {
+                        Text("Playout Controls") //needs equal spacing
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .background(Color.green)
+                    }
+                    Spacer()
+                }
+            }
+            .background(Color.black)
+            .frame(width: 375, height: 825, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) //replace magic numbers with system dimensions
+            
+    }
+    
+    func initialPosition(boundingBox g: GeometryProxy) -> CGSize {
+        CGSize(
+            width: g.size.width * ContentView.fourFifths / 2.0
+                - g.size.width * ContentView.fourFifths / 20.0,
+            height: 0.0
+        )
+    }
+    
+    func position(_ g: GeometryProxy, for i: Int) -> CGSize{
+        let pos = initialPosition(boundingBox: g)
+        
+        return CGSize(
+            width: pos.width - 4.0 + (cos(CGFloat(i) * ContentView.tau
+                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.4),
+            height: pos.width + 75.0 + (sin(CGFloat(i) * ContentView.tau
+                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.4)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct SongCircle: View {
-    var body: some View {
-        VStack {
-            Circle().scaleEffect(0.3, anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.black)
-                .offset(x:0, y: 20)
-            HStack {
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: 20, y: -30)
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: -15, y: -30)
-            }
-            HStack {
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: 20, y: -20)
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: -20, y: -20)
-            }
-            HStack {
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: 90, y: -50)
-                Circle()
-                    .scaleEffect(0.3)
-                    .offset(x: -90, y: -50)
-            }
-        }
-    }
-    
-}
-
-struct FollowPath: View {
-    @State private var flag = false
-    
-    var body: some View {
-        GeometryReader { proxy in
-            ZStack(alignment: .topLeading) {
-                SongCircle().frame(width: 25, height: 25).offset(x: 0, y: 0)
-                    .modifier(FollowEffect(pct: self.flag ? 1 : 0, path: CirclePath.create(in: CGRect(x: 0, y: 0, width: 25, height: 25))))
-            }
-        }
-    }
-}
-
-struct FollowEffect: GeometryEffect {
-    var pct: CGFloat = 0
-    let path: Path
-    var rotate = false
-    
-    var animatableData: CGFloat {
-        get { return pct }
-        set { pct = newValue }
-    }
-    
-    func effectValue(size: CGSize) -> ProjectionTransform {
-        if !rotate {
-            let pt = percentPoint(pct)
-            
-            return ProjectionTransform(CGAffineTransform(translationX: pt.x, y: pt.y))
-        } else {
-            let pt1 = percentPoint(pct)
-            let pt2 = percentPoint(pct - 0.01)
-            
-            let a = pt2.x - pt1.x
-            let b = pt2.y - pt1.y
-            
-            let angle = a < 0 ? atan(Double(b/a)) : atan(Double(b/a)) - Double.pi
-            
-            let transform = CGAffineTransform(translationX: pt1.x, y: pt1.y).rotated(by: CGFloat(angle))
-            
-            return ProjectionTransform(transform)
-        }
-    }
-    
-    func percentPoint(_ percent: CGFloat) -> CGPoint {
-        let pct = percent > 1 ? 0 : (percent < 0 ? 1 : percent)
-        let f = pct > 0.999 ? CGFloat(1-0.001) : pct
-        let t = pct > 0.999 ? CGFloat(1) : pct + 0.001
-        let tp = path.trimmedPath(from: f, to: t)
-        
-        return CGPoint(x: tp.boundingRect.midX, y: tp.boundingRect.midY)
-    }
-}
-
-struct CirclePath: Shape {
-    
-    func path(in rect: CGRect) -> Path {
-        return CirclePath.create(in: rect)
-    }
-    
-    static func create(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.addEllipse(in: CGRect(x: 10, y: 10, width: 10, height: 10))
-        
-        return path
     }
 }
---
+++ MyMusicBoard/MyMusicBoard/README.md
@@ -0,0 +1,38 @@
+#  Kiklo (Name subject to change)
+
+
+//TODO:
+    
+    //Have 3 stationary dots that are blank, user can click into a dot and scroll their spotify (eventually soundcloud & music) account and select a song to be there
+    //The album art will populate the dot and upon pressing the dot it will start a stream on the user's device. music will stream in a playlist dictated by the order of the dots (clockwise) the bar at the bottom will have controls
+    //Have user profile image open a form modal where a user can upload a few images (scaled & sliding upper half) above their long form writing. If user does not choose to fill it out, upon a viewer click it will just darken on tap
+    //Have the playout controls allow changing the song, volume, shuffle?, play/pause
+    //Use TCA to set this all up.
+
+
+//Wendell's thoughts:
+
+    // do TCA first, make sure you get the functionality going
+    // talk to Keith after the UI is built out to see what he thinks from a UX perspective
+    // having a plus button gives CTA, plus button could expand radially into edit, reorder, add, delete
+    // 1st "dot" created starts at 6 o'clock
+        // 2nd "dot" created goes to 6, 1st shifts to 9
+        // this continues & keeps equal distance between dots
+
+//Things I need to decide:
+    
+    // What to call each "dot"
+    // Is there a limit on how many songs someone can put?
+    // How will they be archived? Should it be reset weekly? Weekly based upon date or weekly based upon whenever the user started? I could see a calendar tab that a user can tap into (divided by weeks)
+    // a better name than MyMusicBoard (cycles) (kiklo) <3 (shark weekly)
+    // make it a weekly thing, calendar based
+    // should the image in the middle be a profile image? or an image relating to that week of music
+    // each dot should just play the song, pressing the user's image in the middle will open up their "expression form" where the user can upload a few images & some long form writing if they please. This is optional.
+    // it would be cool to have user's have a QR code for others to view their profile for that week (or a specific week?)
+    
+//Future Tings:
+
+    // Have a calendar view that users can view their archived "kiklos"
+    // have a server that holds user data and can host more than one user lol
+    // have a way to export a "kiklo" to share to other social media platforms (yuck)
+    
