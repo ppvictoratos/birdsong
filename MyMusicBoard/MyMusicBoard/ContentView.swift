4c464543079f2a681bb903ae491063ca40e677db
pete <pete.victoratos@aboutobjects.com>
2020-08-14 14:25:05 -0400

    welp back to square 1
    going to have to redo the UI to temporarily hold some elements that I can build upon to make sure I can get the endpoints working

--- MyMusicBoard/MyMusicBoard/ContentView.swift
+++ MyMusicBoard/MyMusicBoard/ContentView.swift
@@ -10,10 +10,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    static var numCircles = 7
+    static var numCircles = 3
     static var tau = CGFloat(2.0 * 3.14159265358979323846)
     static var fourFifths = CGFloat(0.8)
     static var circleSize = fourFifths/10.0
+    
+    //wendell underlined boundingBox and square.. it seems I need to custom define those to be a circle that is the same size as the user image
     
     var body: some View {
             GeometryReader { boundingBox in
@@ -27,13 +29,14 @@ struct ContentView: View {
                                 .overlay(Circle()
                                             .stroke(Color.white, lineWidth: 5))
                                 .scaleEffect(ContentView.fourFifths - 0.3, anchor: .center)
+                                .offset(x: 35, y: 0)
                         GeometryReader { square in
                             ForEach(0 ..< ContentView.numCircles) { i in
                                 //replace these circles with buttons that allow user to click into and get more data, a hero animation would be great
                                 Image("spoofy")
                                     .frame(
-                                        width: boundingBox.size.width * ContentView.circleSize + 170, //translates on x
-                                        height: boundingBox.size.width * ContentView.circleSize + 15, //scales
+                                        width: boundingBox.size.width * ContentView.circleSize + 300, //translates on x
+                                        height: boundingBox.size.width * ContentView.circleSize + 65, //scales
                                         alignment: .center
                                     ) //circle size
                                     .scaleEffect(0.025)
@@ -46,26 +49,25 @@ struct ContentView: View {
                             height: ContentView.fourFifths * boundingBox.size.width + 10,
                             alignment: .center
                         ) // box size
-                        Spacer()
                     }
                     Text("shneakypete")
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
-                        .offset(x: 0.0 , y: 20)
+                        .offset(x: 50 , y: 20)
                     Spacer()
-                    Button(action: {}) {
-                        Text("Playout Controls") //needs equal spacing
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .background(Color.green)
+                    Button(action: {
+                        
+                    }) {
+                        Text("yo")
                     }
                     Spacer()
-                }
+                    }
             }
             .background(Color.black)
             .frame(width: 375, height: 825, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) //replace magic numbers with system dimensions
-            
     }
+    
+    //this position func should probably utilize the unit circle to lay out the dots
     
     func initialPosition(boundingBox g: GeometryProxy) -> CGSize {
         CGSize(
@@ -80,10 +82,31 @@ struct ContentView: View {
         
         return CGSize(
             width: pos.width - 4.0 + (cos(CGFloat(i) * ContentView.tau
-                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.4),
+                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.45),
             height: pos.width + 75.0 + (sin(CGFloat(i) * ContentView.tau
-                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.4)
+                                        / CGFloat(ContentView.numCircles)) * g.size.width * 0.45)
         )
+    }
+}
+
+struct MediaPlayoutMenu: View {
+    @State private var volume: Double = 0
+    
+    var body: some View { //might need to create a custom button  here
+            Spacer()
+            HStack{
+                Spacer()
+                Image(systemName: "play.fill")
+                Image(systemName: "pause.fill")
+                Image(systemName: "speaker.fill")
+                Slider(value: $volume, in: 0...100, step: 1)
+                //slider
+                Spacer()
+            }
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .background(Color.green)
+    
     }
 }
 
