3caa642d590d3c519d3e4109e48fd3fb1487c067
pete <pete.victoratos@aboutobjects.com>
2020-09-24 10:18:41 -0400

    added project to git

---
+++ SoundEffects/SoundEffects/SoundEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundEffectsApp.swift
+//  SoundEffects
+//
+//  Created by Peter Victoratos on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SoundEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
