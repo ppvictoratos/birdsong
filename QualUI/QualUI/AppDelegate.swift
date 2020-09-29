d37a9f062dadf318a12894536ce8c227ff193bd5
pete <pete.victoratos@aboutobjects.com>
2020-09-28 22:53:06 -0400

    made it better

---
+++ QualUI/QualUI/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  QualUI
+//
+//  Created by Peter Victoratos on 9/28/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundEffects/SoundEffects/NOT MINE.swift
@@ -0,0 +1,8 @@
+//
+//  NOT MINE.swift
+//  SoundEffects
+//
+//  Created by Peter Victoratos on 9/28/20.
+//
+
+import Foundation
