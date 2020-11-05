561d62579cc4b44a4569db2a548e83be4822e7cf
pete <pete.victoratos@aboutobjects.com>
2020-11-05 09:38:03 -0500

    added PF voice memos app

---
+++ A 2NiNG4K/A 2NiNG4K/PF Memo Recorder/AudioPlayerClient/AudioPlayerClient.swift
@@ -0,0 +1,23 @@
+//
+//  AudioPlayerClient.swift
+//  A 2NiNG4K
+//
+//  Created by Peter Victoratos on 11/5/20.
+//
+
+import ComposableArchitecture
+import Foundation
+
+struct AudioPlayerClient {
+  var play: (AnyHashable, URL) -> Effect<Action, Failure>
+  var stop: (AnyHashable) -> Effect<Never, Never>
+
+  enum Action: Equatable {
+    case didFinishPlaying(successfully: Bool)
+  }
+
+  enum Failure: Equatable, Error {
+    case couldntCreateAudioPlayer
+    case decodeErrorDidOccur
+  }
+}
---
+++ A 2NiNG4K/A 2NiNG4K/PF Memo Recorder/Helpers.swift
@@ -0,0 +1,8 @@
+//
+//  Helpers.swift
+//  A 2NiNG4K
+//
+//  Created by Peter Victoratos on 11/4/20.
+//
+
+import Foundation
