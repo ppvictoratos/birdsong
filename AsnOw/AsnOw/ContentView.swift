22ca1f61ff61e6f2870999dc5185e855220e0b7a
pete <pete.victoratos@aboutobjects.com>
2020-12-14 20:57:42 -0500

    made a snowflake!
    yuh

--- AsnOw/AsnOw/ContentView.swift
+++ AsnOw/AsnOw/ContentView.swift
@@ -13,32 +13,6 @@ private let innerRadiusRatio: CGFloat = 0.4
 private let strokeStyle = StrokeStyle(lineWidth: 1, lineJoin: .round)
 private let starLayoutConfig = LayoutGuideConfig.polar(rings: [0, innerRadiusRatio, 1], segments: 12)
 
-struct StarNative: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        let outerRadius = min(rect.height, rect.width) / 2
-        let innerRadius = outerRadius * innerRadiusRatio
-        let center = CGPoint(x: rect.midX, y: rect.midY)
-        let stepAngle = CGFloat(2 * Int(Double.pi) / numSegments) //might be expensive
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        for index in 0..<numSegments {
-            
-            let angle = CGFloat(index) * stepAngle
-            let radius = index.isMultiple(of: 2) ? outerRadius : innerRadius
-            let xOffset = radius * sin(angle)
-            let yOffset = radius * cos(angle)
-            
-            path.addLine(to: CGPoint(x: center.x + xOffset, y: center.y - yOffset))
-        }
-        path.closeSubpath()
-        
-        return path
-    }
-}
-
 struct Star: Shape {
     
     func path(in rect: CGRect) -> Path {
@@ -58,19 +32,66 @@ struct Star: Shape {
     }
 }
 
+struct Polygon: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        var g = starLayoutConfig.layout(in: rect)
+        
+        path.move(g[2, 0])
+        
+        for segment in 1..<g.yCount {
+            path.line(g[2, segment])
+        }
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct Snowflake: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        let numCrystals = 7
+        var layout = LayoutGuide.polar(rect, rings: [0.75], segments: numCrystals)
+        var layoutSmall = layout.reframed(rect.scaled(0.9, at: rect.center), origin: .center)
+        var path = Path()
+        
+        var g = starLayoutConfig.layout(in: rect)
+        
+        path.move(g[2,0])
+        
+        for segment in 0..<numCrystals {
+            path.move(layoutSmall[0, 0, origin: layout[0, segment]])
+            for smallSegment in 0...numSegments {
+                path.line(layoutSmall[0, smallSegment, origin: layout[0, segment]])
+            }
+        }
+        
+        return path
+    }
+}
+
 struct PolarLayoutStarDemo_Previews: PreviewProvider {
     struct PolarLayoutStarDemo_Harness: View {
         
         var body: some View {
             VStack(spacing: 50){
                 Group {
-                    StarNative()
-                        .stroke(style: strokeStyle)
                     Star()
                         .stroke(style: strokeStyle)
                         .layoutGuide(starLayoutConfig)
+                    Polygon()
+                        .stroke(style: strokeStyle)
+                        .layoutGuide(starLayoutConfig)
+                    Snowflake()
+                        .stroke(style: strokeStyle)
+                        .layoutGuide(starLayoutConfig)
+                        .scaleEffect(0.5)
+                    
                 }
-                .frame(150)
+                .frame(250)
             }
         }
     }
@@ -85,9 +106,8 @@ struct PolarLayoutStarDemo_Previews: PreviewProvider {
 
 struct ContentView: View {
     var body: some View {
-        StarNative()
-            .stroke(style: strokeStyle)
-            .padding()
+        Polygon().stroke(style: strokeStyle)
+            .layoutGuide(starLayoutConfig)
     }
 }
 
